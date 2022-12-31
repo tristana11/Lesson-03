ebd2ff9b727f2bc5f766dee3480f0c831bd6927e
Nataliia Levak <nataliialevak@gmail.com>
2022-12-31 17:32:30 +0300

    Add files via upload

--- Lesson03task3.java
+++ Lesson03task3.java
@@ -1,16 +1,16 @@
 public class Lesson03task3 {
     public static void main(String[] args) {
-        int numHourDay = 11;
-        if (numHourDay >= 0 && numHourDay < 6) {
-            System.out.printf("%d - доброй ночи.", numHourDay);
-        } else if (numHourDay >= 6 && numHourDay < 12) {
-            System.out.printf("%d - доброе утро.", numHourDay);
-        } else if (numHourDay >= 12 && numHourDay < 18) {
-            System.out.printf("%d - добрый день.", numHourDay);
-        } else if (numHourDay >= 18 && numHourDay < 24) {
-            System.out.printf("%d - добрый вечер.", numHourDay);
+        int numTimeDay = 6;
+        if (numTimeDay >= 0 && numTimeDay < 6) {
+            System.out.printf("%d - доброй ночи.", numTimeDay);
+        } else if (numTimeDay >= 6 && numTimeDay < 12) {
+            System.out.printf("%d - доброе утро.", numTimeDay);
+        } else if (numTimeDay >= 12 && numTimeDay < 18) {
+            System.out.printf("%d - добрый день.", numTimeDay);
+        } else if (numTimeDay >= 18 && numTimeDay < 24) {
+            System.out.printf("%d - добрый вечер.", numTimeDay);
         } else {
-            System.out.printf("%d - такого часа в сутках нет.", numHourDay);
+            System.out.printf("%d - такого часа в сутках нет.", numTimeDay);
         }
     }
 }
